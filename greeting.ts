8b190f7d76cabf1afc0089cf36f777f048e13a56
md-kudrot-592 <kk.mdkudrot@gmail.com>
2026-06-30 23:08:45 +0600

    Start learning TS

---
+++ greeting.ts
@@ -0,0 +1,6 @@
+function sayGreeting(message: string) {
+    console.log(`The greeting is: ${message}`)
+}
+
+const greetingMessage = "Hello, welcome to the TypeScript project!"
+sayGreeting(greetingMessage)
